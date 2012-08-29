9987ad7ab90911cefbfce8e70e8fa9779af4e456
umpirsky <umpirsky@gmail.com>
2012-08-29 11:32:38 +0200

    Namespace added.

--- Twig/Gettext/Loader/Filesystem.php
+++ Twig/Gettext/Loader/Filesystem.php
@@ -9,12 +9,14 @@
  * file that was distributed with this source code.
  */
 
+namespace Twig\Gettext\Loader;
+
 /**
  * Loads template from the filesystem.
  *
  * @author Саша Стаменковић <umpirsky@gmail.com>
  */
-class Filesystem extends Twig_Loader_Filesystem
+class Filesystem extends \Twig_Loader_Filesystem
 {
     /**
      * Hacked find template to allow loading templates by absolute path.
@@ -25,14 +27,14 @@ class Filesystem extends Twig_Loader_Filesystem
     {
         try {
             parent::findTemplate($name);
-        } catch (Twig_Error_Loader $e) {
+        } catch (\Twig_Error_Loader $e) {
             if (is_file($name)) {
                 return $this->cache[$name] = $name;
             }
         }
         
         if (empty($this->cache)) {
-            throw new Twig_Error_Loader(sprintf('Unable to find template "%s" (looked into: %s).', $name, implode(', ', $this->paths)));
+            throw new \Twig_Error_Loader(sprintf('Unable to find template "%s" (looked into: %s).', $name, implode(', ', $this->paths)));
         } else {
             return array_pop($this->cache);
         }
